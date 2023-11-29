338df7d8dc358f33c9c546625ed93deb7d663453
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-11-30 01:43:01 +0530

    Refactor: generic method to find maximum

--- Maximum.java
+++ Maximum.java
@@ -2,46 +2,23 @@ package com.example.maxMin;
 
 import java.util.Scanner;
 
-public class Maximum{
-        public static void main(String[]args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Please Enter first Integer:");
-        Integer a=new Integer(sc.nextInt());
-        System.out.println("Please Enter second Integer:");
-        Integer b=new Integer(sc.nextInt());
-        System.out.println("Please Enter third Integer:");
-        Integer c=new Integer(sc.nextInt());
-        System.out.println("Maximum among three Integer is:"+ maximumInteger(a,b,c));
+public class Maximum {
+        public static void main(String[] args) {
 
-                System.out.println("Please Enter first Float:");
-                Float af=new Float(sc.nextFloat());
-                System.out.println("Please Enter second Float:");
-                Float bf=new Float(sc.nextFloat());
-                System.out.println("Please Enter third Float:");
-                Float cf=new Float(sc.nextFloat());
-                System.out.println("Maximum among three Float is:"+ maximumFloat(af,bf,cf));
-
+                maximum(3,4,1);
+                maximum(1.2f,3.4f,5.6f);
+                maximum("Apple","Peach","Banana");
         }
-        /*
-        @desc: compute  the maximum Integer among three Integer
-        @param: Integer a,b,c
-        @return : int value of Integer
+
+        /**
+         * @desc: compute the maximum among three Comparable objects
+         * @param: T a, b, c (where T extends Comparable<T>)
+         * @return: Maximum of three Comparable objects
          */
-        public static int  maximumInteger(Integer a,Integer b,Integer c){
-        if(a.compareTo(b)>=0 && a.compareTo(c)>=0)return a.intValue();
-        else if(b.compareTo(a)>=0 && b.compareTo(c)>=0)return b.intValue();
-        else return c.intValue();
-
+        public static <T extends Comparable<T>> void maximum(T a, T b, T c) {
+          T res=a;
+          if(b.compareTo(a)>0) res=b;
+          if(c.compareTo(b)>0) res=c;
+                System.out.println("Maximum among " +a +", "+ b+" and "+c +" is: "+ res);
         }
-        /*
-      @desc: compute  the maximum Float among three Float
-      @param: Float a,b,c
-      @return : float value of maximum Float
-       */
-        public static float  maximumFloat(Float a,Float b,Float c){
-                if(a.compareTo(b)>=0 && a.compareTo(c)>=0)return a.floatValue();
-                else if(b.compareTo(a)>=0 && b.compareTo(c)>=0)return b.floatValue();
-                else return c.floatValue();
-
-        }
-        }
+}
